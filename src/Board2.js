ba713477c71ddc33e917759ef20e62c5d72bcca3
KeslieBox <kesliebox@gmail.com>
2021-08-10 15:00:37 -0500

    gets makeshift version of brick flip working

--- src/Board2.js
+++ src/Board2.js
@@ -2,89 +2,60 @@ import React from 'react'
 import Paddle from './Paddle'
 import Square from './Square'
 
-let bricks = [1, 2, 3, 4, 5, 6]
-let board, rows
-class Board extends React.Component{
+let board
+function Board (){
   
-    generateBoard(columns, rows) {
+    function generateBoard(columns, rows){
         board = [];
-        // while i is less than 40, i = i + 8
-        // for (let i = 0; i < columns*rows; i+=columns) {
-        // debugger
-        // board[i] = board.push(
-        //     <>
-        //     {/* <div className="board-row" key={i}> */}
-        //         {console.log('i, i + columns', i, i + columns)}
-        //         {/* generate row with column number of elements, ie row with 8 elements */}
-        //         {/* send current index and current index plus number of columns ie 8 */}
-        //         {this.generateRow(i, i + columns)}
-        //     {/* </div> */}
-        //     </>
-        //     ); 
-        // }
-        let i
         for (let i = 0; i < rows; i++){
             board[i] = []
 
             for (let j = 1; j <= columns; j++){
-
-                <div className="board-row" key={i}>
+                // <div className="board-row" key={i}>
                     {/* use index of brick to determine where bombs go */}
                     {board[i].push(j)} 
-                </div>
-            
-                
-                // board[i] = board.push(this.generateRow(i, i + columns))
-                // board[i].push(j)              
+                {/* </div>           */}
             }
         }
         return board;
     }
 
-    generateRow(index, max) {
-        rows = [];
-        // index = current 
-        for (index; index < max; index++) {
-            rows.push(index);
-            // rows.push(this.renderSquare(index));
-        }
-        return rows;
-    }
-
-    renderSquare(i) {
+    function handleClick(e){
         debugger
-        return (
-            // <div className='container'>
-                <Square key={i} id='square'/>
-            // </div>
-        )
     }
 
-    render (){
-        
-        this.generateBoard(30, 5)
-        return (
+    // function generateRow(index, max) {
+    //     rows = [];
+    //     // index = current 
+    //     for (index; index < max; index++) {
+    //         rows.push(index);
+    //         // rows.push(this.renderSquare(index));
+    //     }
+    //     return rows;
+    // }
+
+    generateBoard(30, 15)
+
+    return (
         <div className='container'>
-            {board.map((array, i) => {
+            {board.map((rows, i) => {
 
                 return (<div className="board-row" key={i}>
-                {array.map((_, i) => {
-                            
-                    return <>
-                        <Square key={i} id='square'/>
-                    </>
+                {rows.map((_, i) => { 
+                    if (rows[i] % 3 === 0){
+                        return <>
+                            <Square key={i} id='square' name='bomb'/>
+                        </>
+                    } else {    
+                        return <>
+                            <Square key={i} id='square'/>
+                        </>
+                    }
                 })}
                 </div>)
-            })} 
-            
-            {/* <Square id='square'  /> */}
-            <Paddle />
-            
+            })}         
         </div>
-        )
-    }
-
-  
+    )  
 }
 
 
--- src/Square.js
+++ src/Square.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import styled from 'styled-components';
+import Logo from './logo.svg'
 
-class Square extends React.Component {
-    render() {
-        
-        return (
-            <button id='square'  ></button>
-        );
+
+
+function Square(props) {
+    function handleClick(e){
+        debugger
+        // transform brick either to another stage or to a bomb
+        if (e.target.id === 'bomb'){
+            e.target.src = Logo
+        } else {
+
+        }
     }
+        
+    return (
+        props.name && props.name === 'bomb' ? 
+            <>
+            <button id='bomb' onClick={(e) => handleClick(e)} name={props.name}></button>
+            <img id={props.name} onClick={(e) => handleClick(e)} />
+            </> :
+            <>
+            <button id='square' onClick={(e) => handleClick(e)} name={props.name}></button>
+            <img id={props.name} onClick={(e) => handleClick(e)} />
+            </>
+
+        
+    )
 }
 
 export default Square
