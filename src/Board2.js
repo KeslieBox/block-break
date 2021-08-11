b4d0b269300ecbbfd55d3853f7fb7bb0e7dacc02
KeslieBox <kesliebox@gmail.com>
2021-08-11 15:49:41 -0500

    sets up some styling for bricks, click events working minimally

--- src/Board2.js
+++ src/Board2.js
@@ -34,27 +34,41 @@ function Board (){
     //     return rows;
     // }
 
-    generateBoard(30, 15)
+    generateBoard(28,6)
 
     return (
+        <>
+        <img id='dungeon' src='https://images.unsplash.com/photo-1618488629391-e7d08445272b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' alt='dungeon'/>        
+        <img id='brick-wall' src='https://images.unsplash.com/photo-1589890378282-face76ca7e0f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' alt='brick-wall' />
+
         <div className='container'>
+            
             {board.map((rows, i) => {
 
                 return (<div className="board-row" key={i}>
                 {rows.map((_, i) => { 
                     if (rows[i] % 3 === 0){
                         return <>
-                            <Square key={i} id='square' name='bomb'/>
+                            <Square key={i} id='square' name='bomb' />
                         </>
-                    } else {    
-                        return <>
-                            <Square key={i} id='square'/>
-                        </>
+                    } else {
+                        if (rows[i + 1] % 3 === 0 || rows[i - 1] % 3 === 0 ){ 
+                            // debugger   
+                            return <>
+                                <Square key={i} id='square' name={'square'} status={'bombAdjacent'}/>
+                            </>
+                        } else {
+                            return <>
+                                <Square key={i} id='square' name={'square'}/>
+                            </>
+                        }
                     }
                 })}
                 </div>)
-            })}         
+            })} 
+            {/* <img id='dungeon' src='https://images.unsplash.com/photo-1618488629391-e7d08445272b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' alt='dungeon'/>         */}
         </div>
+        </>
     )  
 }
 
--- src/Square.js
+++ src/Square.js
@@ -2,32 +2,53 @@ import React from 'react'
 import styled from 'styled-components';
 import Logo from './logo.svg'
 
-
+// const Button = styled.button `
+//   margin: 40px;
+//   border: 5px outset pink;
+//   &:hover {
+//    background-color: yellow;
+//  }`
 
 function Square(props) {
-    function handleClick(e){
-        debugger
-        // transform brick either to another stage or to a bomb
+    function handleClick(e, status){
+        // flip brick to bomb
         if (e.target.id === 'bomb'){
+            debugger
             e.target.src = Logo
+            // render bomb component, then render homepage or "try again" etc
+        // change brick to some other thing, maybe numbers of bombs it is touching, turn red if it is next to bomb
         } else {
-
+            debugger
+            if (status === 'bombAdjacent'){
+                e.target.style.backgroundColor = 'red'
+            }
         }
     }
         
     return (
         props.name && props.name === 'bomb' ? 
             <>
-            <button id='bomb' onClick={(e) => handleClick(e)} name={props.name}></button>
+            {/* <button id='bomb' onClick={(e) => handleClick(e)} name={props.name}/> */}
             <img id={props.name} onClick={(e) => handleClick(e)} />
             </> :
-            <>
-            <button id='square' onClick={(e) => handleClick(e)} name={props.name}></button>
-            <img id={props.name} onClick={(e) => handleClick(e)} />
-            </>
+            <button id='square' onClick={(e) => handleClick(e, props.status)} name={props.name}></button>  
+            // <img src='' alt='brick' id='square' onClick={(e) => handleClick(e, props.status)} name={props.name}/>   
+ 
+    )
+
+    // return (
+    //     props.name && props.name === 'bomb' ? 
+    //     <>
+    //         <button id='bomb' onClick={(e) => handleClick(e)} name={props.name}></button>
+    //         <img id={props.name} onClick={(e) => handleClick(e)} />
+    //     </> :
+    //     <>
+    //         <button id='square' onClick={(e) => handleClick(e)} ></button>
+    //         <img id='sqImage' onClick={(e) => handleClick(e)} />
+    //     </>
 
         
-    )
+    // )
 }
 
 export default Square
